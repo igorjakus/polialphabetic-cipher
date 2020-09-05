fafe51a43bacc829edf0dc2d9b72e2667450d1cc
igorjakus <igorjakus3@gmail.com>
2020-09-05 11:54:24 +0200

    Code clean

--- vigenere.cpp
+++ vigenere.cpp
@@ -1,46 +1,50 @@
 #include <iostream>
 #include <string>
 
-std::string encrypt(std::string text, std::string keyWord)
+std::string encrypt(std::string plaintext, std::string keyWord)
 {
-	std::string result = "";
-	int key = 0;
+	std::string encrypted = "";
+	int shift { 0 };
 
-	for (unsigned int i = 0, k = 0; i < text.length(); i++, k++)
+	for (unsigned int i { 0 }, k { 0 }; i < plaintext.length(); i++, k++)
 	{
-		while (text[i] == char(32)) i++;
+		// Skipping spaces
+		while (plaintext[i] == char(32)) i++;
 
+		// Going to the start of the key when it ends
 		if (k >= keyWord.length())
 			k = 0;
 
+		// Converting key into shift numbers
 		if (isupper(keyWord[k])) 
-			key = int(keyWord[k]) - 64;
+			shift = int(keyWord[k]) - 64;
 		else 
-			key = int(keyWord[k]) - 96;
+			shift = int(keyWord[k]) - 96;
 
-		if (isupper(text[i]))
-			result += char(int(text[i] + key - 65) % 26 + 65);
+		// Encrypting plaintext
+		if (isupper(plaintext[i]))
+			encrypted += char(int(plaintext[i] + shift - 65) % 26 + 65);
 		else
-			result += char(int(text[i] + key - 97) % 26 + 97);
+			encrypted += char(int(plaintext[i] + shift - 97) % 26 + 97);
 	}
 	
-	return result;
+	return encrypted;
 }
 
 int main()
 {
-	//Wpisanie tekstu do zaszyfrowania
-	std::string text;
-	std::cout << "Podaj tekst do zaszyfrowania: ";
+	// Enter text
+	std::string text = "";
+	std::cout << "Enter the text to be encrypted: ";
 	std::getline(std::cin, text);
 
-	//Wpisanie klucza
-	std::string key;
-	std::cout << "Podaj klucz: ";
+	// Enter key
+	std::string key = "";
+	std::cout << "Enter the encryption key: ";
 	std::cin >> key;
 
-	//Wyswietlenie zaszyfrowanego tekstu
-	std::cout << "Zaszyfrowany tekst: " << encrypt(text, key) << std::endl;
+	// Display encrypted text
+	std::cout << "Encrypted text: " << encrypt(text, key);
 
 	return 0;
 }
