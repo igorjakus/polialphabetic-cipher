acbf88301c2ac82eba22643b1dfa3ee46e59ed9a
igorjakus <igorjakus3@gmail.com>
2020-09-15 11:51:32 +0200

    Added pytest tests

--- test.py
+++ test.py
@@ -1,66 +1,43 @@
-import unittest
 from vigenere import encrypt, decrypt
 
-class TestVigenereCipher(unittest.TestCase):
 
-    def test_encryption(self):
-        key = 'SnAKE'      
-        plaintext = 'meet me at elephant lake'
-        expected =  'FSFERXOUPQXDILSMZBVJ'
-        self.assertEqual(encrypt(plaintext, key), expected)
-
-        key = 'zZz'      
-        plaintext = 'A12'
-        expected =  'A'
-        self.assertEqual(encrypt(plaintext, key), expected)
-
-        key = 'elOnmusk'      
-        plaintext = 'tesla'
-        expected =  'YQHZN'
-        self.assertEqual(encrypt(plaintext, key), expected)
-
-        key = 'peN'      
-        plaintext = 'pineapple'
-        expected =  'FNBUFDFQS'
-        self.assertEqual(encrypt(plaintext, key), expected)
-
-        key = '22222222'      
-        plaintext = 'fkfkfkfkkf'
-        expected =  None
-        self.assertEqual(encrypt(plaintext, key), expected)
-
-        key = 'zwadew'      
-        plaintext = '12931995415941045'
-        expected =  None
-        self.assertEqual(encrypt(plaintext, key), expected)
-
-        key = 'gwYadawrgjSAffaz'      
-        plaintext = 'dqw3rtkwjrtlw3tf3w4tffad'
-        expected =  'KNVSXLTBYDEXZLXTMCZE'
-        self.assertEqual(encrypt(plaintext, key), expected)
-
-    def test_decryption(self):
-        
-        key = 'snake'
-        encrypted = 'FSFERXOUPQXDILSMZBVJ'
-        expected =  'MEETMEATELEPHANTLAKE'
-        self.assertEqual(decrypt(encrypted, key), expected)
-
-        key = 'gwYadawrgjSAffaz'
-        encrypted = 'KNVSXLTBYDEXZLXTMCZE'
-        expected =  'DQWRTKWJRTLWTFWTFFAD'
-        self.assertEqual(decrypt(encrypted, key), expected)
-
-        key = 'pEn'
-        encrypted = 'FNBUFDFQS'
-        expected =  'PINEAPPLE'
-        self.assertEqual(decrypt(encrypted, key), expected)
-
-        key = 'zZz'      
-        encrypted = 'a'
-        expected =  'A'
-        self.assertEqual(decrypt(encrypted, key), expected)
+def test_encryption_1():
+    key = 'snake'
+    plaintext = 'meet me at elephant lake '
+    expected = 'FSFERXOUPQXDILSMZBVJ'
+    assert encrypt(plaintext, key) == expected
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_encryption_2():
+    key = 'abc'
+    plaintext = 'cbacbacba'
+    expected = 'DDDDDDDDD'
+    assert encrypt(plaintext, key) == expected
+
+
+def test_encryption_none():
+    key = ''
+    plaintext = 'example'
+    expected = None
+    assert encrypt(plaintext, key) == expected
+
+
+def test_decryption_1():
+    key = 'snake'
+    encrypted = 'FSFERXOUPQXDILSMZBVJ'
+    expected = 'MEETMEATELEPHANTLAKE'
+    assert decrypt(encrypted, key) == expected
+
+
+def test_decryption_2():
+    key = 'zab'
+    encrypted = 'aaaaaaaaa'
+    expected = 'ABCABCABC'
+    assert decrypt(encrypted, key) == expected
+
+
+def test_decryption_none():
+    key = 'example'
+    encrypted = ''
+    expected = None
+    assert decrypt(encrypted, key) == expected
